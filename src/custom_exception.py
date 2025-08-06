18f70b182ea5c384296fdc367204d9593e2c3c88
rxhit17 <rohitgaikwadstud@gmail.com>
2025-08-06 18:54:33 +0530

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,21 +1,20 @@
 import traceback
+import sys
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail):
+
+    def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = self.get_detailed_message(error_message, error_detail)
+        self.error_message = self.get_detailed_error_message(error_message,error_detail)
 
     @staticmethod
-    def get_detailed_message(error_message, error_detail):
-        tb = traceback.extract_tb(error_detail.__traceback__)
-        if tb:
-            file_name = tb[-1].filename
-            line_number = tb[-1].lineno
-        else:
-            file_name = "Unknown"
-            line_number = "Unknown"
+    def get_detailed_error_message(error_message , error_detail:sys):
 
-        return f"Error in {file_name}, line {line_number}: {error_message} | Cause: {str(error_detail)}"
+        _, _, exc_tb = traceback.sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
 
+        return f"Error in {file_name} , line {line_number} : {error_message}"
+    
     def __str__(self):
         return self.error_message
